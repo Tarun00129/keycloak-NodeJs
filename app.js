7d9a66ec53477dd4e29d8969bbb7bce93a4f70ce
Tarun <tarun@ectl.in>
2024-04-25 11:36:26 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const cors = require('cors');
+const express = require('express');
+const keycloak = require('./middlewares/keycloak'); // Keycloak
+const menuItems = require('./middlewares/menuItems');
+
+const port = 8000;
+
+// Routes
+const testRoutes = require('./routes/test');
+
+const errorHandler = (error, req, res, next) => {
+  const status = error.status || 422;
+  res.status(status).send(error.message);
+}
+
+const app = express();
+
+app.use(keycloak.middleware());
+app.use(express.json());
+app.use(cors());
+
+// Register routes
+// app.use('/api', testRoutes);
+app.use('/api',menuItems);
+app.use(errorHandler);
+
+app.get('/api/secure', keycloak.protect('realm:admin'), (req, res) => {
+  res.json({ user: "**user**", project: 'keycloak_project', release: '1.0.0', description: 'API for appuser' });
+})
+
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
+});
